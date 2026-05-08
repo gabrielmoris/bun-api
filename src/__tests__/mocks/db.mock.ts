3ff9ba439ec281d81c6c1d73475515c5c3dc9060
gabriel <gabrielcmoris@gmail.com>
2026-05-08 23:05:48 +0200

    Refactor bookmark creation tests for improved readability and structure

--- src/__tests__/mocks/db.mock.ts
+++ src/__tests__/mocks/db.mock.ts
@@ -1,8 +1,12 @@
 import { mock } from "bun:test";
 
-export const mockConnectDB = () =>
+export const mockConnectDB = (shouldFail = false) =>
   mock.module("../../db/mongo.ts", () => ({
-    connectDB: mock(async () => ({ fake: "connection" })),
+    connectDB: shouldFail
+      ? mock(async () => {
+          throw new Error("Mocked DB Error");
+        })
+      : mock(async () => ({ fake: "connection" })),
   }));
 
 export const findOneMock = mock(async (_query: any): Promise<any> => null);
