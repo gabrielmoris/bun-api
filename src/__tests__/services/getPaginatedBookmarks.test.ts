97b2073018dff6023d754a1302d625d516be5499
gabriel <gabrielcmoris@gmail.com>
2026-05-09 20:54:17 +0200

    Add tests for getPaginatedBookmarks service to validate pagination and error handling

---
+++ src/__tests__/services/getPaginatedBookmarks.test.ts
@@ -0,0 +1,82 @@
+import { expect, test, describe, beforeAll, mock } from "bun:test";
+import {
+  mockBookmarkModel,
+  mockConnectDB,
+  findMock,
+  sortMock,
+  skipMock,
+  limitMock,
+  leanMock,
+  countDocumentsMock,
+} from "../mocks/db.mock";
+import { mockedBookmarks } from "../mocks/bookmarks.mock";
+
+const { getPaginatedBookmarks } =
+  await import("../../services/getPaginatedBookmarks");
+
+describe("Bookmarks creation", () => {
+  beforeAll(() => {
+    mock.restore();
+    mockConnectDB();
+    mockBookmarkModel();
+
+    findMock.mockClear();
+    sortMock.mockClear();
+    skipMock.mockClear();
+    limitMock.mockClear();
+    leanMock.mockClear();
+    countDocumentsMock.mockClear();
+  });
+
+  test("It gets the first page with limit 1", async () => {
+    leanMock.mockResolvedValueOnce([mockedBookmarks[0]]);
+    countDocumentsMock.mockResolvedValueOnce(3);
+
+    const result = await getPaginatedBookmarks(1, 1);
+
+    expect(findMock).toHaveBeenCalledWith({});
+    expect(skipMock).toHaveBeenCalledWith(0);
+    expect(limitMock).toHaveBeenCalledWith(1);
+
+    expect(result).toMatchObject({
+      total: 3,
+      bookmarks: [mockedBookmarks[0]],
+    });
+  });
+
+  test("It gets the second page with limit 1", async () => {
+    leanMock.mockResolvedValueOnce([mockedBookmarks[1]]);
+    countDocumentsMock.mockResolvedValueOnce(3);
+
+    const result = await getPaginatedBookmarks(2, 1);
+
+    expect(findMock).toHaveBeenCalledWith({});
+    expect(skipMock).toHaveBeenCalledWith(0);
+    expect(limitMock).toHaveBeenCalledWith(1);
+
+    expect(result).toMatchObject({
+      total: 3,
+      bookmarks: [mockedBookmarks[1]],
+    });
+  });
+
+  test("Sends proper error structure when it fails", async () => {
+    mockConnectDB(true);
+    mockBookmarkModel();
+
+    const result = await getPaginatedBookmarks(1, 1);
+
+    expect(result).toEqual({
+      error: {
+        code: "UNKNOWN_ERROR",
+        message: "Mocked DB Error",
+        details: [
+          {
+            field: "unknown",
+            message: "Unknown error happened retrieving Bookmarks",
+          },
+        ],
+      },
+    });
+  });
+});
