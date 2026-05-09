2bce3a55d899bba47083215144870054260499a5
gabriel <gabrielcmoris@gmail.com>
2026-05-09 20:54:38 +0200

    Add tests for getPaginatedBookmarks service to validate pagination and error handling

--- src/__tests__/mocks/bookmarks.mock.ts
+++ src/__tests__/mocks/bookmarks.mock.ts
@@ -4,3 +4,24 @@ export const mockedBookmark = {
   description: "test description",
   tags: ["test-tag-1", "test-tag-2"],
 };
+
+export const mockedBookmarks = [
+  {
+    url: "https://www.test1.com",
+    title: "test1",
+    description: "test description 1",
+    tags: ["test-tag-1"],
+  },
+  {
+    url: "https://www.test2.com",
+    title: "test2",
+    description: "test description 2",
+    tags: ["test-tag-1", "test-tag-2"],
+  },
+  {
+    url: "https://www.test3.com 3",
+    title: "test3",
+    description: "test description",
+    tags: ["test-tag-1", "test-tag-2", "test-tag-3"],
+  },
+];
--- src/__tests__/mocks/db.mock.ts
+++ src/__tests__/mocks/db.mock.ts
@@ -6,10 +6,18 @@ export const mockConnectDB = (shouldFail = false) =>
       ? mock(async () => {
           throw new Error("Mocked DB Error");
         })
-      : mock(async () => ({ fake: "connection" })),
+      : connectDBMock,
   }));
 
+export const connectDBMock = mock(async () => ({ fake: "connection" }));
 export const findOneMock = mock(async (_query: any): Promise<any> => null);
+export const leanMock = mock((): Promise<any[]> => Promise.resolve([]));
+export const limitMock = mock((_limit: number) => ({ lean: leanMock }));
+export const skipMock = mock((_skip: number) => ({ limit: limitMock }));
+export const sortMock = mock((_sort: any) => ({ skip: skipMock }));
+export const findMock = mock((_query: any) => ({ sort: sortMock }));
+
+export const countDocumentsMock = mock(async () => 3);
 
 export const createMock = mock(async (bookmark: any) => ({
   _id: "fake-id-123",
@@ -21,5 +29,7 @@ export const mockBookmarkModel = () =>
     default: {
       findOne: findOneMock,
       create: createMock,
+      find: findMock,
+      countDocuments: countDocumentsMock,
     },
   }));
